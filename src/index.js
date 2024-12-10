0a3bd8099c9724462b569e1a1a02352741ae72b4
fotomain <foto888999@gmail.com>
2024-12-10 18:30:14 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+// import reportWebVitals from './reportWebVitals';
+
+const apolloCache = new InMemoryCache({
+    typePolicies: {
+        Query: {
+            fields: {
+                clients: {
+                    merge(existing, incoming) {
+                        return incoming;
+                    },
+                },
+                projects: {
+                    merge(existing, incoming) {
+                        return incoming;
+                    },
+                },
+            },
+        },
+    },
+});
+
+const apolloClient = new ApolloClient({
+    uri: 'https://render2-mern.onrender.com/api/v1/graphql',
+    cache: apolloCache,
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+
+      <ApolloProvider client={apolloClient}>
+            <App />
+      </ApolloProvider>
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
